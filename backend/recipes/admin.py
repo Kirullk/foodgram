6924f012da8cd90427e055921b8f22c81c91bb3e
kirill <kirill14659@gmail.com>
2026-09-19 21:17:44 +0800

    Добавлена админка для рецептов

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Ingredient, Tag
+from .models import Ingredient, Tag, Recipe
+from users.models import User
 
 
 @admin.register(Tag)
@@ -13,3 +14,19 @@ class TagAdmin(admin.ModelAdmin):
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
     search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author_name')
+    search_fields = ('name', 'author__first_name')
+    list_filter = ('tags',)
+    readonly_fields = ('favorites_count',)
+
+    def author_name(self, obj):
+        return obj.author.first_name
+    author_name.short_description = 'Автор'
+
+    def favorites_count(self, obj):
+        return obj.favorited_by.count()
+    favorites_count.short_description = 'В избранном'
