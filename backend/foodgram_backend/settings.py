51ccfada31887d4419cfe9593eda5d87a6292c78
kirill <kirill14659@gmail.com>
2026-09-23 20:09:00 +0800

    Изменены неправильные импорты

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -14,6 +14,10 @@ ALLOWED_HOSTS = os.getenv(
     'ALLOWED_HOSTS', 'localhost,127.0.0.1'
 ).split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS', ''
+).split(',')
+
 SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET')
 
 INSTALLED_APPS = [
