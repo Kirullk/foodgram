db203e2481e13e289761f6f3d8152c7270bc6256
kirill <kirill14659@gmail.com>
2026-09-23 18:53:51 +0800

    Поменял маршруты сериалайзеров в настройках

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -129,8 +129,8 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     'SERIALIZERS': {
-        'user': 'users.serializers.UserSerializer',
-        'current_user': 'users.serializers.UserSerializer',
-        'user_create': 'users.serializers.UserCreateSerializer',
+        'user': 'api.serializers.UserSerializer',
+        'current_user': 'api.serializers.UserSerializer',
+        'user_create': 'api.serializers.UserCreateSerializer',
     },
 }
